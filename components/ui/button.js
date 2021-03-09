0932b5bbdd9747fe3bad7598b50b20cb00ef19e4
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-09 07:33:24 +0100

    add event list

---
+++ components/ui/button.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./button.module.css";
+
+function Button(props) {
+  return (
+    <Link href={props.link}>
+      <a className={classes.btn}>{props.children}</a>
+    </Link>
+  );
+}
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }

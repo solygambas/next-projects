309fd08f797e9588bf1eb730adc10d9b3e6b5dec
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-22 12:16:26 +0100

    add pre-rendering and data fetching to next events

--- 01-nextevents/pages/index.js
+++ 01-nextevents/pages/index.js
@@ -1,13 +1,22 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 }
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
---
+++ components/layout/layout.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import MainHeader from "./main-header";
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+
+export default Layout;

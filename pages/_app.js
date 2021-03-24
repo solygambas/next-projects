805b281672e49ee3e98e8fe01057918e81223574
solygambas <51904909+solygambas@users.noreply.github.com>
2021-03-24 19:59:07 +0100

    add next blog setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,7 @@
-import Head from "next/head";
-
-import Layout from "../components/layout/layout";
-import { NotificationContextProvider } from "../store/notification-context";
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <NotificationContextProvider>
-      <Layout>
-        <Head>
-          <title>NextEvents</title>
-          <meta
-            name="description"
-            content="Find a lot of great events that allow you to grow as a developer"
-          />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
-    </NotificationContextProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp

f6690b419d69dc53f16e6a83c53904901a1c8a0f
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-13 16:49:34 +0200

    add strapi and cloudinary

---
+++ 05-djevents/djevents-backend/config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+});
---
+++ 05-djevents/djevents-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '581aa296-c3cc-4263-988d-abd0e4eb10d7'
+};

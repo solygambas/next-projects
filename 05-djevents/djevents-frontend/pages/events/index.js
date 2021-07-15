d75974f8a87ace26e5c5a5f714a412e6cc0177d2
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-15 07:39:51 +0200

    add pagination

--- 05-djevents/djevents-frontend/pages/events/index.js
+++ 05-djevents/djevents-frontend/pages/events/index.js
@@ -1,8 +1,9 @@
 import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
-import { API_URL } from "@/config/index";
+import Pagination from "@/components/Pagination";
+import { API_URL, PER_PAGE } from "@/config/index";
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout>
       <h1>Events</h1>
@@ -10,15 +11,32 @@ export default function EventsPage({ events }) {
       {events.map((singleEvent) => (
         <EventItem key={singleEvent.id} singleEvent={singleEvent} />
       ))}
+      <Pagination page={page} total={total} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
-  const events = await res.json();
+// handle pagination
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+  // Fetch total
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
+  // Fetch events
+  const eventRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const events = await eventRes.json();
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   };
 }
+// export async function getStaticProps() {
+//   const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
+//   const events = await res.json();
+//   return {
+//     props: { events },
+//     revalidate: 1,
+//   };
+// }

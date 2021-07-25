08c2cf2635697a6459f21638685ad2b7065a2707
solygambas <51904909+solygambas@users.noreply.github.com>
2021-07-25 14:42:58 +0200

    fix package.json

--- 06-devspace/pages/404.js
+++ 06-devspace/pages/404.js
@@ -13,7 +13,6 @@ export default function NotFoundPage() {
           className="bg-gray-800 rounded-2xl"
         />
         <h1 className="text-6xl my-5">Whoops!</h1>
-
         <h2 className="text-4xl text-gray-400 mb-5">
           This page does not exist
         </h2>

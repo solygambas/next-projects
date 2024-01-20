dbfdb0ffd68af84cd1f42194f47d5dbd55e7139b
solygambas <51904909+solygambas@users.noreply.github.com>
2024-01-20 10:34:29 +0100

    feat: fetching data

--- 00-playground/12-dagny-media/README.md
+++ 00-playground/12-dagny-media/README.md
@@ -11,6 +11,6 @@ A quick intro to NextJS 13 features.
 - using the routing system and creating nested routes.
 - customizing the layout, using the Metadata API and Google Fonts.
 - creating a header and discovering React Server Components.
-- fetching data.
+- fetching data from GitHub API.
 
 Based on [Next.js 13 Crash Course | App Directory, React Server Components & More](https://www.youtube.com/watch?v=Y6KDk5iyrYE) by Brad Traversy - Traversy Media (2023).
--- 00-playground/12-dagny-media/app/code/repos/page.jsx
+++ 00-playground/12-dagny-media/app/code/repos/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
 async function fetchRepos() {
   const response = await fetch("https://api.github.com/users/solygambas/repos");
   const repos = await response.json();
@@ -6,8 +9,32 @@ async function fetchRepos() {
 
 const ReposPage = async () => {
   const repos = await fetchRepos();
-  console.log(repos);
-  return <div>{repos[0].name}</div>;
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ReposPage;

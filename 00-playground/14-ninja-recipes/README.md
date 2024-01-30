69eff560a132e6e16b7584c671e2bf8659b0fa87
solygambas <51904909+solygambas@users.noreply.github.com>
2024-01-30 09:29:42 +0100

    feat: add cards

--- 00-playground/14-ninja-recipes/README.md
+++ 00-playground/14-ninja-recipes/README.md
@@ -9,5 +9,7 @@ A quick intro to shadcn/ui, using TypeScript and TailwindCSS.
 ## Features
 
 - setting up the project.
+- fetching data, using a locally hosted JSON server.
+- creating a Card component.
 
 Based on [Shadcn UI Crash Course](https://www.youtube.com/playlist?list=PL4cUxeGkcC9h1NXLUuiAQ7c4UtdEInqma) by Shaun Pelling - Net Ninja (2023).
--- 00-playground/14-ninja-recipes/src/app/page.tsx
+++ 00-playground/14-ninja-recipes/src/app/page.tsx
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <main>Home</main>;
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+interface Recipe {
+  id: string;
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const response = await fetch("http://localhost:4000/recipes");
+  return response.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p className="mt-0">Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }

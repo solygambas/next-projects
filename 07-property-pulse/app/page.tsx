c4b59d13d4a83e60ac1d0ab8f966e468b546bdaa
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-07 08:21:19 +0200

    feat: add recent properties

--- 07-property-pulse/app/page.tsx
+++ 07-property-pulse/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 }
---
+++ 07-property-pulse/components/HomeProperties.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
+const HomeProperties = () => {
+  const recentProperties = properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Recent Properties
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentProperties.length === 0 ? (
+              <p>No Properties Found</p>
+            ) : (
+              recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))
+            )}
+            ;
+          </div>
+        </div>
+      </section>
+      <section className="m-auto max-w-lg my-10 px-6">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
+  );
+};
+export default HomeProperties;

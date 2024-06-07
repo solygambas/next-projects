86b531b1887d9b7b69d61dd4faa7f5246a51b9ec
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-07 08:07:12 +0200

    feat: add PropertyCard

--- 07-property-pulse/app/properties/page.tsx
+++ 07-property-pulse/app/properties/page.tsx
@@ -1,12 +1,20 @@
-import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 export default function PropertiesPage() {
   return (
-    <main>
-      <div>
-        <h1 className="text-3xl">Properties</h1>
-        <Link href="/">Go Home</Link>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <div>No properties found</div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
       </div>
-    </main>
+    </section>
   );
 }
---
+++ 07-property-pulse/components/PropertyCard.tsx
@@ -0,0 +1,139 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
+
+type Location = {
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+};
+
+type Rates = {
+  nightly?: number;
+  weekly?: number;
+  monthly?: number;
+};
+
+type SellerInfo = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+type PropertyCardProps = {
+  property: {
+    _id: string;
+    owner: string;
+    name: string;
+    type: string;
+    description: string;
+    location: Location;
+    beds: number;
+    baths: number;
+    square_feet: number;
+    amenities: string[];
+    rates: Rates;
+    seller_info: SellerInfo;
+    images: string[];
+    is_featured: boolean;
+    createdAt: string;
+    updatedAt: string;
+  };
+};
+
+const PropertyCard = ({ property }: PropertyCardProps) => {
+  const { nightly, weekly, monthly } = property.rates;
+  const getRateDisplay = () => {
+    if (monthly) {
+      return `$${monthly.toLocaleString()}/mo`;
+    } else if (weekly) {
+      return `$${weekly.toLocaleString()}/wk`;
+    } else if (nightly) {
+      return `$${nightly.toLocaleString()}/nt`;
+    }
+  };
+
+  return (
+    <div className="rounded-xl shadow-md relative">
+      <Image
+        src={`/images/properties/${property.images[0]}`}
+        alt=""
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-auto rounded-t-xl"
+      />
+      <div className="p-4">
+        <div className="text-left md:text-center lg:text-left mb-6">
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
+        </div>
+        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
+          {getRateDisplay()}
+        </h3>
+
+        <div className="flex justify-center gap-4 text-gray-500 mb-4">
+          <p>
+            <FaBed className="inline mr-2" /> {property.beds}{" "}
+            <span className="md:hidden lg:inline">Beds</span>
+          </p>
+          <p>
+            <FaBath className="inline mr-2" /> {property.baths}{" "}
+            <span className="md:hidden lg:inline">Baths</span>
+          </p>
+          <p>
+            <FaRulerCombined className="inline mr-2" />
+            {property.square_feet.toLocaleString()}{" "}
+            <span className="md:hidden lg:inline">sqft</span>
+          </p>
+        </div>
+
+        <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
+          {nightly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" />
+              Nightly
+            </p>
+          )}
+          {weekly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" />
+              Weekly
+            </p>
+          )}
+          {monthly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Monthly
+            </p>
+          )}
+        </div>
+
+        <div className="border border-gray-100 mb-5"></div>
+
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="flex align-middle gap-2 mb-4 lg:mb-0">
+            <FaMapMarker className="text-lg text-orange-700 mt-1" />
+            <span className="text-orange-700">
+              {" "}
+              {property.location.city} {property.location.state}{" "}
+            </span>
+          </div>
+          <Link
+            href={`/properties/${property._id}`}
+            className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+          >
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PropertyCard;

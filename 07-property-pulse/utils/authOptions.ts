7f77925d183f820c679c8a56c27d0e77de009e2c
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-10 14:25:10 +0200

    feat: handle new user and session

--- 07-property-pulse/utils/authOptions.ts
+++ 07-property-pulse/utils/authOptions.ts
@@ -1,5 +1,7 @@
 import { AuthOptions, Profile, Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -17,9 +19,24 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async signIn({ profile }: { profile?: Profile }) {
+      await connectDB();
+      const userExists = await User.findOne({ email: profile?.email });
+      if (!userExists) {
+        const username = profile?.name?.slice(0, 20);
+        await User.create({
+          email: profile?.email,
+          username,
+          image: profile?.image,
+        });
+      }
       return true;
     },
     async session({ session }: { session: Session }) {
+      if (session.user) {
+        await connectDB();
+        const user = await User.findOne({ email: session.user.email });
+        session.user.id = user?._id.toString() as string;
+      }
       return session;
     },
   },

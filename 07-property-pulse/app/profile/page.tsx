e255b7f1b7808ab1eaf956446a38b5e11da8c93a
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-13 11:06:52 +0200

    feat: display user listings

--- 07-property-pulse/app/profile/page.tsx
+++ 07-property-pulse/app/profile/page.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import Spinner from "@/components/Spinner";
+import { PropertyInterface } from "@/models/Property";
 
 import profileDefault from "@/assets/images/profile.png";
 
@@ -13,6 +16,30 @@ const ProfilePage = () => {
     name: profileName,
     email: profileEmail,
   } = session?.user || {};
+
+  const [properties, setProperties] = useState<PropertyInterface[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchUserProperties = async (userId: string) => {
+      if (!userId) return;
+      try {
+        const response = await fetch(`/api/properties/user/${userId}`);
+        if (response.status === 200) {
+          const data = await response.json();
+          setProperties(data);
+        }
+      } catch (error) {
+        console.error("Error fetching properties", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (session?.user?.id) fetchUserProperties(session.user.id);
+  }, [session]);
+
+  const handleDeleteProperty = async (propertyId: string) => {};
 
   return (
     <section className="bg-blue-50">
@@ -40,60 +67,50 @@ const ProfilePage = () => {
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/a1.jpg"
-                    alt="Property 1"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 1</p>
-                  <p className="text-gray-600">Address: 123 Main St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div className="mb-10">
-                <a href="/property.html">
-                  <img
-                    className="h-32 w-full rounded-md object-cover"
-                    src="/images/properties/b1.jpg"
-                    alt="Property 2"
-                  />
-                </a>
-                <div className="mt-2">
-                  <p className="text-lg font-semibold">Property Title 2</p>
-                  <p className="text-gray-600">Address: 456 Elm St</p>
-                </div>
-                <div className="mt-2">
-                  <a
-                    href="/add-property.html"
-                    className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                  >
-                    Edit
-                  </a>
-                  <button
-                    className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
+              {!loading && properties.length === 0 && (
+                <p>You have no listings</p>
+              )}
+
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                properties.map((property) => (
+                  <div key={property._id} className="mb-10">
+                    <Link href={`/properties/${property._id}`}>
+                      <Image
+                        className="h-32 w-full rounded-md object-cover"
+                        src={property.images[0]}
+                        width={500}
+                        height={100}
+                        priority={true}
+                        alt={property.name}
+                      />
+                    </Link>
+                    <div className="mt-2">
+                      <p className="text-lg font-semibold">{property.name}</p>
+                      <p className="text-gray-600">
+                        Address: {property.location.street},{" "}
+                        {property.location.city}, {property.location.state}
+                      </p>
+                    </div>
+                    <div className="mt-2">
+                      <Link
+                        href={`/properties/${property._id}/edit`}
+                        className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => handleDeleteProperty(property._id)}
+                        className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+                        type="button"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>

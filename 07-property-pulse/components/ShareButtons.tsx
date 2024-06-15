243f886f1fa742c3f2f696b4f43d898106496441
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-15 08:16:18 +0200

    feat: add share buttons

--- 07-property-pulse/components/ShareButtons.tsx
+++ 07-property-pulse/components/ShareButtons.tsx
@@ -1,11 +1,52 @@
-import { FaShare } from "react-icons/fa";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  EmailShareButton,
+  EmailIcon,
+} from "react-share";
 import { PropertyInterface } from "@/models/Property";
 
 const ShareButtons = ({ property }: { property: PropertyInterface }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/property/${property._id}`;
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="mr-2" /> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">
+        Share This Property:
+      </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          hashtag={`#${property.type.replace(/\s/g, "")}ForRent`}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`${property.type.replace(/\s/g, "")}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          separator=":: "
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          subject={property.name}
+          body={`Check out this property listing: ${shareUrl}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   );
 };
 export default ShareButtons;

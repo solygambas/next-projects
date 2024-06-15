4ce4afb7b0d26ca3ea8b25a7ca9f49953ace6d2f
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-15 06:50:30 +0200

    feat: add bookmarks api route

---
+++ 07-property-pulse/app/api/bookmarks/route.ts
@@ -0,0 +1,36 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+export const POST = async (req: Request, res: Response) => {
+  try {
+    await connectDB();
+    const { propertyId } = await req.json();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    const { userId } = sessionUser;
+    const user = await User.findOne({ _id: userId });
+    let isBookmarked = user.bookmarks.includes(propertyId);
+    let message;
+    if (isBookmarked) {
+      user.bookmarks.pull(propertyId);
+      message = "Property removed from bookmarks";
+      isBookmarked = false;
+    } else {
+      user.bookmarks.push(propertyId);
+      message = "Property added to bookmarks";
+      isBookmarked = true;
+    }
+    await user.save();
+    return new Response(JSON.stringify({ message, isBookmarked }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("An error occurred", { status: 500 });
+  }
+};

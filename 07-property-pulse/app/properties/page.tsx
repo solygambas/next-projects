e89a5141aebc3dafc71bdabeac3b16395c023c72
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-16 15:58:01 +0200

    feat: display search results

--- 07-property-pulse/app/properties/page.tsx
+++ 07-property-pulse/app/properties/page.tsx
@@ -1,4 +1,5 @@
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import { PropertyInterface } from "@/models/Property";
 import { fetchProperties } from "@/utils/requests";
 
@@ -8,18 +9,25 @@ export default async function PropertiesPage() {
     return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
   });
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <div>No properties found</div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          {properties.length === 0 ? (
+            <div>No properties found</div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 }
--- 07-property-pulse/app/properties/search-results/page.tsx
+++ 07-property-pulse/app/properties/search-results/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { PropertyInterface } from "@/models/Property";
+import PropertyCard from "@/components/PropertyCard";
+import Spinner from "@/components/Spinner";
+import PropertySearchForm from "@/components/PropertySearchForm";
 
 const SearchResultsPage = () => {
   const [properties, setProperties] = useState<PropertyInterface[]>([]);
@@ -33,6 +38,39 @@ const SearchResultsPage = () => {
     fetchProperties();
   }, [location, propertyType]);
 
-  return <div>SearchResultsPage</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <Link
+              href="/properties"
+              className="flex items-center text-blue-500 hover:underline mb-3"
+            >
+              <FaArrowAltCircleLeft className="mr-2 mb-1" />
+              Back to Properties
+            </Link>
+            <h1 className="text-3xl font-bold mb-4">Search Results</h1>
+            {properties.length === 0 ? (
+              <div>No search results found</div>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 export default SearchResultsPage;

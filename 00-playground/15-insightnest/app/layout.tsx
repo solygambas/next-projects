bc362cc7d3d96085ac10b94528dec5b004153e6f
solygambas <51904909+solygambas@users.noreply.github.com>
2024-06-19 10:10:28 +0200

    feat: add sidebar

--- 00-playground/15-insightnest/app/layout.tsx
+++ 00-playground/15-insightnest/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className="flex">
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <Sidebar />
+          </div>
+          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
+        </div>
       </body>
     </html>
   );
---
+++ 00-playground/15-insightnest/components/Sidebar.tsx
@@ -0,0 +1,65 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <Command className="bg-secondary rounded-none">
+      <CommandInput placeholder="Type a command or search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>
+            <LayoutDashboard className="mr-2 h-4 w-4" />
+            <Link href="/">Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Newspaper className="mr-2 h-4 w-4" />
+            <Link href="/posts">Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className="mr-2 h-4 w-4" />
+            <Link href="#">Categories</Link>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className="mr-2 h-4 w-4" />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+};
+
+export default Sidebar;
